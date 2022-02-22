7fd50e43e9f52b459009acb8d7110e4e9153cb03
drabika <abigeldrabik@gmail.com>
2022-02-22 18:51:13 +0100

    week2 folytatása

---
+++ week2/src/main/java/com/example/week2/student/StudentService.java
@@ -0,0 +1,30 @@
+package com.example.week2.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+        import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Csaba",
+                        "janocsikcsaba@gmail.com",
+                        LocalDate.of(1998, Month.JUNE, 30),
+                        23
+                ),
+                new Student(
+                2L,
+                "Abigél",
+                "abigeldrabik@gmail.com",
+                LocalDate.of(2000, Month.MARCH, 25),
+                21
+        )
+        );
+    }
+}
